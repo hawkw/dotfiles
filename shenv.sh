294c980928a95f3c58c0a3967b6b3d4e08107b57
Eliza Weisman <eliza@buoyant.io>
2017-07-19 08:44:39 -0500

    env tweaks

--- shenv.sh
+++ shenv.sh
@@ -12,12 +12,8 @@ export PATH="$PATH:/usr/bin:/bin:/usr/sbin:/sbin"
 export PATH="$PATH:/usr/texbin"
 # latex bin on El Capitan
 export PATH="$PATH:/Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin"
-# Julia
-export PATH="$PATH:/Applications/Julia-0.3.0.app/Contents/Resources/julia/bin"
-# golang
-export PATH="$PATH:/usr/local/bin/go:$HOME/Development/go/bin"
-# perl 5?
-export PATH="$PATH:$HOME/perl5/bin"
+# # Julia
+# export PATH="$PATH:/Applications/Julia-0.3.0.app/Contents/Resources/julia/bin"
 # llvm
 export PATH="$PATH:/usr/local/opt/llvm/bin/"
 # cross-compiled binutils
@@ -33,40 +29,30 @@ export PATH="$PATH:$HOME/opt/bin/"
 # fi
 
 export PATH="$PATH:/usr/local/bin/rust-os-gdb/bin"
-export PATH="$PATH:${HOME}/.local/bin:$HOME/.cabal/bin"
-# Other path environment variables ############################################
-export GOPATH="$HOME/Development/go"
-# Rust sources (racer needs this)
-export RUST_SRC_PATH="/usr/local/src/rust/src/"
+export PATH="$PATH:$HOME/.local/bin:$HOME/.cabal/bin"
+# Rust source path for Racer
+export  RUST_SRC_PATH="$HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src"
+# Go #########################################################################
+export GOPATH="$HOME/Code/go"
+export GOROOT="/usr/local/opt/go/libexec"
+export PATH="$PATH:$GOPATH/bin"
+export PATH="$PATH:$GOROOT/bin"
 
 # ssh
 export SSH_KEY_PATH="$HOME/.ssh/id.rsa"
 # LeJOS EV3
 export EV3_HOME="/opt/lejos-ev3"
+
 # OpenSSL bits
 # export OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include
 # export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
 
-export GH_TOKEN="6353fb099fc52f91067d5ee1c97e60af292a2612"
+export GH_TOKEN="50d27932f63f848fc91e49aed480715c1ac877a5"
 export CARGO_TOKEN="h12iJk5JKCcVY1CR9OQpGpXdpQldzPoY"
 export VERSIONEYE_API_KEY="022c68589ca94418c558"
 
-# Disable emoji in Homebrew (this makes Hyperterm unhappy) ###################
-# [ "$TERM_PROGRAM" = "Hyper" ] && export HYPER=1
+
 # if [ $HYPER ]
 # then
 #   # export HOMEBREW_NO_EMOJI=1
 # fi
-
-# Enable Terminal.app folder icons ###########################################
-[ "$TERM_PROGRAM" = "Apple_Terminal" ] && export TERMINALAPP=1
-if [ $TERMINALAPP ]
-then
-  set_terminal_app_pwd() {
-    # Tell Terminal.app about each directory change.
-    printf '\e]7;%s\a' "$(echo "file://$HOST$PWD" | sed -e 's/ /%20/g')"
-  }
-fi
-[ -s ~/.lastpwd ] && [ "$PWD" = "$HOME" ] && \
-  builtin cd `cat ~/.lastpwd` 2>/dev/null
-[ $TERMINALAPP ] && set_terminal_app_pwd
