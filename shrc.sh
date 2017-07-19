4e4ef6bbf0d11143b27a414eeeb34480f42b62d3
Eliza Weisman <eliza@buoyant.io>
2017-07-19 09:03:07 -0500

    handle term compatibility in shrc

--- shrc.sh
+++ shrc.sh
@@ -9,3 +9,17 @@ if [[ -n $SSH_CONNECTION ]]; then
 else
    export EDITOR='atom-beta'
 fi
+
+### Hyper Compatability #############################################
+[ "$TERM_PROGRAM" = "Hyper" ] && export HYPER=1
+if [ $HYPER ] then
+    # (see https://github.com/zeit/hyperterm/issues/360)
+    export LANG="en_US.UTF-8"
+    export LC_COLLATE="en_US.UTF-8"
+    export LC_CTYPE="en_US.UTF-8"
+    export LC_MESSAGES="en_US.UTF-8"
+    export LC_MONETARY="en_US.UTF-8"
+    export LC_NUMERIC="en_US.UTF-8"
+    export LC_TIME="en_US.UTF-8"
+    export LC_ALL="en_US.UTF-8"
+fi
--- zshenv
+++ zshenv
@@ -2,14 +2,5 @@
 #   ZSH environment variables
 #   by Eliza Weisman
 #
-
+skip_global_compinit=1
 source $HOME/.shenv.sh
-# Hyperterm Compatability (see https://github.com/zeit/hyperterm/issues/360)
-export LANG="en_US.UTF-8"
-export LC_COLLATE="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-export LC_MESSAGES="en_US.UTF-8"
-export LC_MONETARY="en_US.UTF-8"
-export LC_NUMERIC="en_US.UTF-8"
-export LC_TIME="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
