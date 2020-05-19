afef64e277bd2581c8c493ffc2992c30a37f8c84
Eliza Weisman <eliza@buoyant.io>
2020-05-19 09:35:27 -0700

    more nix
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -1,11 +1,1 @@
-{
-  allowUnfree = true;
-  #   packageOverrides = pkgs:
-  #     with pkgs; {
-  #       vscode = pkgs.vscode.override { isInsiders = true; };
-  #       myPackages = pkgs.buildEnv {
-  #         name = "my-packages";
-  #         paths = [ gdb vscode ];
-  #       };
-  #     };
-}
+{ allowUnfree = true; }
--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -15,15 +15,42 @@
   home.stateVersion = "20.03";
 
   # Packages managed by Home Manager
-  home.packages = [ pkgs.vscode ];
+  home.packages = with pkgs; [
+    nixfmt
+    direnv
+    vscode
+    rustup
+  ];
 
   programs.vscode = {
     enable = true;
     extensions = (with pkgs.vscode-extensions; [
+      # nix syntax highlighting
       bbenoist.Nix
-      # ms-azuretools.vscode-docker
       ms-vscode-remote.remote-ssh
-    ]);
+      ms-azuretools.vscode-docker
+      ms-kubernetes-tools.vscode-kubernetes-tools
+      # wal colorscheme
+      cmschuetz12.wal
+      # rust-analyzer
+      matklad.rust-analyzer
+      WakaTime.vscode-wakatime
+    ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+      name = "remote-ssh-edit";
+      publisher = "ms-vscode-remote";
+      version = "0.47.2";
+      sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+    }
+    # {
+    #   name = "rewrap";
+    #   publisher = "stkb";
+    #   version = "1.0.1";
+    # }
+    # {
+    #   name = "remote-containers";
+    #   publisher = "ms-vscode-remote";
+    #   version = "0.117.1";
+    # }
+      ];
   };
-
 }
