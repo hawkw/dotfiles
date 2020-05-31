5776c64cc33d0311049b11c06b46f2d721c8b250
Eliza Weisman <eliza@buoyant.io>
2020-05-31 10:46:14 -0700

    stuff
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -13,44 +13,101 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "20.03";
-
-  # Packages managed by Home Manager
+  
   home.packages = with pkgs; [
+    # (pkgs.vscode-with-extensions.override {
+    #   vscodeExtensions = (with pkgs.vscode-extensions; [
+    #       # nix syntax highlighting
+    #       bbenoist.Nix
+    #       ms-vscode-remote.remote-ssh
+    #       ms-azuretools.vscode-docker
+    #       ms-kubernetes-tools.vscode-kubernetes-tools
+    #       # cmschuetz12.wal
+    #       # rust-analyzer
+    #       matklad.rust-analyzer
+    #       WakaTime.vscode-wakatime
+    #     ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+    #       name = "remote-ssh-edit";
+    #       publisher = "ms-vscode-remote";
+    #       version = "0.47.2";
+    #       sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+    #     }
+
+    #     # wal colorscheme
+    #     {
+    #       name = "wal-theme";
+    #       publisher = "dlasagno";
+    #       version = "1.0.4";
+    #       sha256 = "1kn38anhhac3l1yanmhjqwv8ga8iaijyy7f56c8ifyzgqqz5bbxf";
+    #     }
+    #     # {
+    #     #   name = "rewrap";
+    #     #   publisher = "stkb";
+    #     #   version = "1.0.1";
+    #     # }
+    #     # {
+    #     #   name = "remote-containers";
+    #     #   publisher = "ms-vscode-remote";
+    #     #   version = "0.117.1";
+    #     # }
+    #     ];
+    # })
     vscode
     rustup
   ];
 
-  programs.vscode = {
-    enable = true;
-    extensions = (with pkgs.vscode-extensions; [
-      # nix syntax highlighting
-      bbenoist.Nix
-      ms-vscode-remote.remote-ssh
-      ms-azuretools.vscode-docker
-      ms-kubernetes-tools.vscode-kubernetes-tools
-      # wal colorscheme
-      cmschuetz12.wal
-      # rust-analyzer
-      matklad.rust-analyzer
-      WakaTime.vscode-wakatime
-    ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-      name = "remote-ssh-edit";
-      publisher = "ms-vscode-remote";
-      version = "0.47.2";
-      sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-    }
-    # {
-    #   name = "rewrap";
-    #   publisher = "stkb";
-    #   version = "1.0.1";
-    # }
-    # {
-    #   name = "remote-containers";
-    #   publisher = "ms-vscode-remote";
-    #   version = "0.117.1";
-    # }
-      ];
-  };
+  # programs.vscode = {
+  #   enable = true;
+  #   extensions = (with pkgs.vscode-extensions; [
+  #     # nix syntax highlighting
+  #     bbenoist.Nix
+  #     ms-vscode-remote.remote-ssh
+  #     ms-azuretools.vscode-docker
+  #     ms-kubernetes-tools.vscode-kubernetes-tools
+  #     # wal colorscheme
+  #     cmschuetz12.wal
+  #     # rust-analyzer
+  #     matklad.rust-analyzer
+  #     WakaTime.vscode-wakatime
+  #   ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+  #     name = "remote-ssh-edit";
+  #     publisher = "ms-vscode-remote";
+  #     version = "0.47.2";
+  #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+  #   }
+  #   # {
+  #   #   name = "rewrap";
+  #   #   publisher = "stkb";
+  #   #   version = "1.0.1";
+  #   # }
+  #   # {
+  #   #   name = "remote-containers";
+  #   #   publisher = "ms-vscode-remote";
+  #   #   version = "0.117.1";
+  #   # }
+  #     ];
+  #   # userSettings = {
+  #   #   "editor.fontFamily" = "Iosevka";
+  #   #   "editor.fontLigatures" = "'calt', 'dlig', 'ss08'";
+  #   #   "editor.renderWhitespace" = "boundary";
+  #   #   "workbench.colorTheme" = "Wal";
+  #   #   "git.autofetch" = true;
+  #   #   "files.associations" = {
+  #   #     "zshrc" = "shellscript";
+  #   #   };
+  #   #   "rewrap.autoWrap.enabled" = true;
+  #   #   "rewrap.reformat" = true;
+  #   #   "editor.formatOnSave" = true;
+  #   #   "[markdown]" =  {
+  #   #     "files.trimTrailingWhitespace" = false;
+  #   #     "editor.formatOnSave" = false;
+  #   #     "editor.trimAutoWhitespace" = false;
+  #   #     "rewrap.reformat" = false;
+  #   #   };
+  #   #   "wal.autoReload" = true;
+  #   #   # "window.titleBarStyle" = "custom";
+  #   # };
+  # };
 
   programs.starship = {
     enable = true;
