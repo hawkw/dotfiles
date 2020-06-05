b68e3254fac7eba96b53babd2a79bbaea938a762
Eliza Weisman <eliza@buoyant.io>
2020-06-04 17:34:27 -0700

    rm unused
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -54,59 +54,6 @@
 
   home.sessionVariables = { EDITOR = "code --wait"; };
 
-  # programs.vscode = {
-  #   enable = true;
-  #   extensions = (with pkgs.vscode-extensions; [
-  #     # nix syntax highlighting
-  #     bbenoist.Nix
-  #     ms-vscode-remote.remote-ssh
-  #     ms-azuretools.vscode-docker
-  #     ms-kubernetes-tools.vscode-kubernetes-tools
-  #     # wal colorscheme
-  #     cmschuetz12.wal
-  #     # rust-analyzer
-  #     matklad.rust-analyzer
-  #     WakaTime.vscode-wakatime
-  #   ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-  #     name = "remote-ssh-edit";
-  #     publisher = "ms-vscode-remote";
-  #     version = "0.47.2";
-  #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-  #   }
-  #   # {
-  #   #   name = "rewrap";
-  #   #   publisher = "stkb";
-  #   #   version = "1.0.1";
-  #   # }
-  #   # {
-  #   #   name = "remote-containers";
-  #   #   publisher = "ms-vscode-remote";
-  #   #   version = "0.117.1";
-  #   # }
-  #     ];
-  #   # userSettings = {
-  #   #   "editor.fontFamily" = "Iosevka";
-  #   #   "editor.fontLigatures" = "'calt', 'dlig', 'ss08'";
-  #   #   "editor.renderWhitespace" = "boundary";
-  #   #   "workbench.colorTheme" = "Wal";
-  #   #   "git.autofetch" = true;
-  #   #   "files.associations" = {
-  #   #     "zshrc" = "shellscript";
-  #   #   };
-  #   #   "rewrap.autoWrap.enabled" = true;
-  #   #   "rewrap.reformat" = true;
-  #   #   "editor.formatOnSave" = true;
-  #   #   "[markdown]" =  {
-  #   #     "files.trimTrailingWhitespace" = false;
-  #   #     "editor.formatOnSave" = false;
-  #   #     "editor.trimAutoWhitespace" = false;
-  #   #     "rewrap.reformat" = false;
-  #   #   };
-  #   #   "wal.autoReload" = true;
-  #   #   # "window.titleBarStyle" = "custom";
-  #   # };
-  # };
-
   #############################################################################
   ## Services                                                                 #
   #############################################################################
