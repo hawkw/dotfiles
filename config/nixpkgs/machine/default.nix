9f3711feaf5c2f6442c7dfc925898f2c77fab4e1
Eliza Weisman <eliza@buoyant.io>
2020-06-08 11:19:56 -0700

    document how per-machine configs work
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- config/nixpkgs/machine/default.nix
+++ config/nixpkgs/machine/default.nix
@@ -7,4 +7,7 @@ mkMerge [
     inherit (import ./noctis.nix { })
     ;
   })
+  # To add new per-machine configs, add a `mkIf` here similar to the one above.
+  # Then, those configs will be applied only if the hostname matches the new
+  # machine's hostname.
 ]
