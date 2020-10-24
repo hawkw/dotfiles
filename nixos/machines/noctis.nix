398f1f65bcd65957990b55f60df5cb3ee86ad458
Eliza Weisman <eliza@buoyant.io>
2020-10-24 12:16:56 -0700

    lolsob
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- nixos/machines/noctis.nix
+++ nixos/machines/noctis.nix
@@ -15,5 +15,15 @@
 
   environment.systemPackages =
     let unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
-    in [ unstable.openrgb ];
+    in with pkgs; [ unstable.openrgb linuxPackages_latest.perf perf-tools ];
+
+  ## HAHA LOL GNOME BULLSHIT
+  programs = {
+    # without dconf you can't change settings in gnome-terminal, so you are
+    # stuck with extremely broken font rendering. this is because gnome is
+    # extremely well designed.
+    dconf.enable = true;
+    # Used specifically for its (quite magical) "copy as html" function.
+    gnome-terminal.enable = true;
+  };
 }
