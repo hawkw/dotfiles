cd279307aa4425d4e6553948a5d0c428410e4ee6
Eliza Weisman <eliza@buoyant.io>
2021-09-29 10:20:34 -0700

    track renaming gnome3 -> gnome
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- nixos/roles/gnome3.nix
+++ nixos/roles/gnome3.nix
@@ -7,14 +7,14 @@
       layout = "us";
       displayManager.gdm.enable = true;
       displayManager.gdm.wayland = true;
-      desktopManager.gnome3.enable = true;
+      desktopManager.gnome.enable = true;
       displayManager.defaultSession = "gnome";
     };
     dbus.packages = [ pkgs.gnome3.dconf ];
     udev.packages = [ pkgs.gnome3.gnome-settings-daemon ];
 
     # Enable gnome3 components
-    gnome3 = {
+    gnome = {
       # Sushi, a quick previewer for Nautilus
       sushi.enable = true;
     };
@@ -29,7 +29,7 @@
 
   ### gnome-keyring #########################################################
   # enable the Gnome keyring
-  services.gnome3.gnome-keyring.enable = true;
+  services.gnome.gnome-keyring.enable = true;
   # enable gnome keyring unlock on login
   security.pam.services = { login.enableGnomeKeyring = true; };
 }
