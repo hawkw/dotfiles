ab6d173649b584b7c23594c493c86dfb305b3ffc
Eliza Weisman <eliza@buoyant.io>
2021-10-29 10:53:00 -0700

    don't show xmm regs in gdb-dashboard by default
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- config/nixpkgs/role/devbox.nix
+++ config/nixpkgs/role/devbox.nix
@@ -22,5 +22,11 @@
   programs.gdb.dashboard = {
     enable = true;
     enablePygments = true;
+
+    extraConfig = ''
+      dashboard -layout assembly breakpoints expressions history memory registers source stack threads variables
+      dashboard registers -style column-major True
+      dashboard registers -style list 'rax rbx rcx rdx rsi rdi rbp rsp r8 r9 r10 r11 r12 r13 r14 r15 rip eflags cs ss ds es fs gs fs_base gs_base k_gs_base cr0 cr2 cr3 cr4 cr8 efe msxr'
+    '';
   };
 }
