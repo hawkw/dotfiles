f5f9f7c45c6ac8122390f64a3eb5ee5a277f813f
Eliza Weisman <eliza@buoyant.io>
2022-05-27 10:39:44 -0700

    move more pkgs to devbox
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- config/nixpkgs/role/devbox.nix
+++ config/nixpkgs/role/devbox.nix
@@ -19,23 +19,29 @@
     valgrind
   ];
 
-  # Nice terminal UI for gdb
-  programs.gdb.dashboard = {
-    enable = true;
-    enablePygments = true;
+  # Dev tools with extra configuration
+  programs = {
 
-    extraConfig = ''
-      dashboard -layout assembly breakpoints expressions history memory registers source stack threads variables
-      dashboard registers -style column-major True
-      dashboard registers -style list 'rax rbx rcx rdx rsi rdi rbp rsp r8 r9 r10 r11 r12 r13 r14 r15 rip eflags cs ss ds es fs gs fs_base gs_base k_gs_base cr0 cr2 cr3 cr4 cr8 efe msxr'
-    '';
-  };
+    # Nice terminal UI for gdb
+    gdb.dashboard = {
+      enable = true;
+      enablePygments = true;
 
-  # `just` --- a command runner
-  programs.just = {
-    enable = true;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
-    enableFishIntegration = true;
+      extraConfig = ''
+        dashboard -layout assembly breakpoints expressions history memory registers source stack threads variables
+        dashboard registers -style column-major True
+        dashboard registers -style list 'rax rbx rcx rdx rsi rdi rbp rsp r8 r9 r10 r11 r12 r13 r14 r15 rip eflags cs ss ds es fs gs fs_base gs_base k_gs_base cr0 cr2 cr3 cr4 cr8 efe msxr'
+      '';
+    };
+
+    # `just` --- a command runner
+    just = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+    };
+
+    jq.enable = true;
   };
 }
