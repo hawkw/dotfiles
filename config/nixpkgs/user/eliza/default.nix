6cdfe0fc0a0fee2ffad3525ea7938e1118201ddc
Eliza Weisman <eliza@buoyant.io>
2022-12-26 09:39:00 -0800

    wait git stuff is moved now

--- config/nixpkgs/user/eliza/default.nix
+++ config/nixpkgs/user/eliza/default.nix
@@ -113,16 +113,6 @@ in rec {
       enable = true;
       enableAliases = true;
     };
-
-    # custom git configs
-    git = {
-      enable = true;
-      user = {
-        name = user.name;
-        email = user.email;
-        privateConfig = ./git.private.nix;
-      };
-    };
   };
 
   programs = {
